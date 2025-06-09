583ed9fc981f79fd4e2e77d2ac9434c7af1c5ec6
Your Name <kavithapalanisamy6627@gmail.com>
2025-06-09 15:49:55 +0530

    index

---
+++ python/condition.py
@@ -0,0 +1,25 @@
+a = 10
+b = 20
+c = 5
+if a > b and a > c:
+    print("a is Greater")
+elif b>a and b > c:
+    print("b is greater")
+else:
+    print("c is Greater")
+for i in range(1,6,2):
+        print(i)
+
+        def add():
+              print(10+20)
+        add()
+        list=[1,2,4,5,7,8,9];
+        for i in list:
+            print(i)  
+        print(type(tuple))   
+
+        dictionary ={"name":"kavi","password":"13545"} 
+        print(dictionary["name"])
+        print(type(dictionary))
+        for key in dictionary:
+             print(dictionary[key])
